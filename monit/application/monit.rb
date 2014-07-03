59980a44198989b1a7bcda75050cdf78153b6208
David Gillies <david.gillies@bigcommerce.com>
2014-07-03 21:43:06 +1000

    whitespace cleanup

--- monit/application/monit.rb
+++ monit/application/monit.rb
@@ -2,10 +2,10 @@ require 'pp'
 
 class MCollective::Application::Monit<MCollective::Application
 
-	description "Monit agent"
+  description "Monit agent"
 
 
-	usage <<-EOF
+  usage <<-EOF
 mco monit CMD
 
 where CMD is one of:
@@ -20,58 +20,57 @@ where CMD is one of:
     reload             - Reinitialize monit
 EOF
 
-    def post_option_parser(configuration)
-		if (ARGV.empty?)
-			STDERR.puts("command not specified")
-			exit 1
-		end
-		configuration[:cmd] = ARGV.shift.intern
-		
-		if ARGV.size > 0
-			configuration[:name] = ARGV.shift
-		end	
-	end
+  def post_option_parser(configuration)
+    if (ARGV.empty?)
+      STDERR.puts("command not specified")
+      exit 1
+    end
+    configuration[:cmd] = ARGV.shift.intern
 
-    def validate_configuration(configuration)
-		all_commands = [:start,:stop,:restart,:monitor,:unmonitor,:status,:summary,:validate,:reload]
-		requires_name = [:start,:stop,:restart,:monitor,:unmonitor]
+    if ARGV.size > 0
+      configuration[:name] = ARGV.shift
+    end
+  end
+
+  def validate_configuration(configuration)
+    all_commands = [:start,:stop,:restart,:monitor,:unmonitor,:status,:summary,:validate,:reload]
+    requires_name = [:start,:stop,:restart,:monitor,:unmonitor]
         cmd = configuration[:cmd]
         args = configuration[:args] ? configuration[:args] : []
-		raise "Unsupported command #{cmd}, use one of\n#{all_commands.join("\n")}\n" unless all_commands.include?(configuration[:cmd])
-		raise "#{cmd} requires service name" unless (!requires_name.include?(configuration[:cmd]) or configuration.has_key?(:name))
+    raise "Unsupported command #{cmd}, use one of\n#{all_commands.join("\n")}\n" unless all_commands.include?(configuration[:cmd])
+    raise "#{cmd} requires service name" unless (!requires_name.include?(configuration[:cmd]) or configuration.has_key?(:name))
+  end
+
+  def main
+    mc = rpcclient("monit")
+    mc.progress = false
+    mc.send(configuration[:cmd], configuration).each do |resp|
+      d = resp[:data]
+      if resp[:statuscode]
+        if d[:error]
+          puts("#{resp[:sender]} : #{d[:error]}")
+        else
+          if d[:result]
+            puts("#{resp[:sender]} : ")
+            d[:result].each_pair do |type,v|
+              puts("  #{type}:")
+              d[:result][type].each_pair do |name,vv|
+                if vv.kind_of?(String)
+                  puts("    #{name}=#{vv}")
+                elsif vv.kind_of?(Hash)
+                  puts("    #{name}")
+                  vv.each_pair do |kkk,vvv|
+                    puts("      #{kkk}=#{vvv}")
+                  end
+                end
+              end
+            end
+          end
+        end
+      else
+        puts("#{resp[:sender]} : Invalid response (no data)")
+      end
     end
-
-
-	def main
-   		mc = rpcclient("monit")
-		mc.progress = false
-		mc.send(configuration[:cmd], configuration).each do |resp|
-			d = resp[:data]
-			if resp[:statuscode]
-				if d[:error]
-					puts("#{resp[:sender]} : #{d[:error]}")
-				else
-					if d[:result]
-						puts("#{resp[:sender]} : ")
-						d[:result].each_pair do |type,v|
-							puts("  #{type}:")
-							d[:result][type].each_pair do |name,vv|
-								if vv.kind_of?(String)
-									puts("    #{name}=#{vv}")
-								elsif vv.kind_of?(Hash)
-									puts("    #{name}")
-									vv.each_pair do |kk,vv|
-										puts("      #{kk}=#{vv}")
-									end
-								end
-							end
-						end
-					end
-				end
-			else
-				puts("#{resp[:sender]} : Invalid response (no data)")
-			end
-		end
-		mc.disconnect
-	end
+    mc.disconnect
+  end
 end
